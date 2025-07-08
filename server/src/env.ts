adfc80b19e1a62c93baf7fd2d9d89d709a506afe
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-07 22:36:05 -0300

    feat: Connection with DB configuration

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,7 +1,8 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
 const envScheme = z.object({
-    PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url().startsWith('postgres://'),
 })
 
 export const env = envScheme.parse(process.env)
