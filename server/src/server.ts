cedae7ba6312219611e0bc7d0b649652a2bd5d08
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-07 22:08:00 -0300

    feat: Configuration biome

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,23 @@
-import {fastify} from 'fastify'
-
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
 import {
-    serializerCompiler,
-    validatorCompiler,
-    type ZodTypeProvider
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-
-
-import { fastifyCors} from '@fastify/cors'
 import { env } from './env.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-    origin:'http://localhost:5173'
+  origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-
-app.get('/health',() => {
-    return 'ok'
+app.get('/health', () => {
+  return 'ok'
 })
 
-
-app.listen({port: env.PORT }).then(() => {
-    console.log('HTTP server run')
-})
+app.listen({ port: env.PORT })
