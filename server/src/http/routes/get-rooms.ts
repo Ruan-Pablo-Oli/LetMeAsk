30457580a5a74038d6f74f2b5ee1716eec965fa1
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-08 20:06:08 -0300

    update(get-rooms): Selecting fields id and name in rooms

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -5,7 +5,10 @@ import { schema } from '../../db/schema/index.ts'
 export const getRoomsRoute:FastifyPluginCallbackZod =  (app) => {
     app.get('/rooms', async () =>{
 
-        const results = await db.select().from(schema.rooms).orderBy(schema.rooms.createdAt)
+        const results = await db.select({
+            id: schema.rooms.id,
+            name:schema.rooms.name
+        }).from(schema.rooms).orderBy(schema.rooms.createdAt)
 
         return results
     })
