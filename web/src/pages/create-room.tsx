a22da18278c29cb4ca24b25f55b7ce98ee13ada6
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-09 16:46:04 -0300

    feat: Create pages create-room and room

---
+++ web/src/pages/create-room.tsx
@@ -0,0 +1,11 @@
+export function CreateRoom() {
+  return (
+    <div>
+      <div>CreateRoom</div>
+      <a className="underline" href="/room">
+        {' '}
+        Acessar Sala
+      </a>
+    </div>
+  )
+}
