c6cc3222376e223ef8319dcf0e7c40124de95db7
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-11 14:56:26 -0300

    feat: Created route create-room

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -22,4 +23,5 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
 app.listen({ port: env.PORT })
