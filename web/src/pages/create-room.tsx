f213024efdd6cc2a339806ff4c79b40d78cccb2a
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-11 16:29:33 -0300

    update(create-room): Add buttons and link rooms with click

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { ArrowRight } from 'lucide-react'
 import { Link } from 'react-router-dom'
+import { Badge } from '@/components/ui/badge'
 import {
   Card,
   CardContent,
@@ -8,11 +9,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { dayjs } from '@/lib/dayjs'
 
 type GetRoomsAPIResponse = Array<{
   id: string
   name: string
-  questionsCount: number
+  questionCount: number
   createdAt: string
 }>
 
@@ -46,11 +48,20 @@ export function CreateRoom() {
                         key={room.id}
                         to={`rooms/${room.id}`}
                       >
-                        <div className="flex-1">
+                        <div className="flex flex-1 flex-col gap-1">
                           <h3 className="font-medium">{room.name}</h3>
+
+                          <div className="flex items-center gap-2">
+                            <Badge className="text-xs" variant="secondary">
+                              {dayjs(room.createdAt).toNow()}
+                            </Badge>
+                            <Badge className="text-xs" variant="secondary">
+                              {room.questionCount} perguntas(s){' '}
+                            </Badge>
+                          </div>
                         </div>
 
-                        <span className="flex items-center gap-3 text-small">
+                        <span className="flex items-center gap-1 text-small">
                           Entrar
                           <ArrowRight className="size-3" />
                         </span>
