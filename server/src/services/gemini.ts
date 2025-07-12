6f1a3a30ed99a595c0247b18235e8f9e0b978531
Ruan Pablo <ruanpablooliveira20@gmail.com>
2025-07-12 20:17:25 -0300

    feat: Creating route for recive audio data

---
+++ server/src/services/gemini.ts
@@ -0,0 +1,30 @@
+import { GoogleGenAI } from '@google/genai'
+import { env } from '../env.ts'
+
+const gemini = new GoogleGenAI({
+  apiKey: env.GEMINI_API_KEY,
+})
+
+const model = 'gemini-2.5-flash'
+
+export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: 'Transcreva o aúdio para português do brasil, seja preciso e natural na descrição. Mantenha a pontuação adequada e divida o texto em parágrafos quando for adequado.',
+      },
+      {
+        inlineData: {
+          mimeType,
+          data: audioAsBase64,
+        },
+      },
+    ],
+  })
+
+  if (!response.text) {
+    throw new Error('Não foi possível converter o áudio')
+  }
+  return response.text
+}
